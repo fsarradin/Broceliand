e0470568f47bc316a565ce4287c41a3f0a3a6e44
François Sarradin <fsarradin@gmail.com>
2011-12-26 23:04:24 +0100

    added constraint about login uniqueness

--- src/main/java/net/kerflyn/broceliand/model/User.java
+++ src/main/java/net/kerflyn/broceliand/model/User.java
@@ -1,5 +1,6 @@
 package net.kerflyn.broceliand.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -13,6 +14,7 @@ public class User {
 
     private String name;
 
+    @Column(unique = true)
     private String login;
 
     public Long getId() {
