868cd13ea0617feb413eb4886c94f50f9416465d
François Sarradin <fsarradin@gmail.com>
2011-12-28 01:58:51 +0100

    user login

--- src/main/java/net/kerflyn/broceliand/controller/LoginController.java
+++ src/main/java/net/kerflyn/broceliand/controller/LoginController.java
@@ -1,11 +1,52 @@
 package net.kerflyn.broceliand.controller;
 
-/**
- * Created by IntelliJ IDEA.
- * User: fsarradin
- * Date: 28/12/11
- * Time: 01:37
- * To change this template use File | Settings | File Templates.
- */
+import com.google.inject.Inject;
+import net.kerflyn.broceliand.model.User;
+import net.kerflyn.broceliand.service.UserService;
+import org.simpleframework.http.Form;
+import org.simpleframework.http.Request;
+import org.simpleframework.http.Response;
+import org.simpleframework.http.Status;
+import org.simpleframework.http.session.Session;
+import org.simpleframework.util.lease.LeaseException;
+import org.stringtemplate.v4.ST;
+
+import java.io.IOException;
+
+import static net.kerflyn.broceliand.util.Templates.buildTemplate;
+
 public class LoginController {
+
+    private UserService userService;
+
+    @Inject
+    public LoginController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public void render(Request request, Response response) throws IOException {
+        ST template = buildTemplate("public/login.html");
+        response.getPrintStream().append(template.render());
+    }
+
+    @SuppressWarnings("unchecked")
+    public void render(Request request, Response response, String action) throws IOException {
+        if ("connect".equals(action)) {
+            Form form = request.getForm();
+            User user = userService.findByLogin(form.get("login"));
+            if (user != null) {
+                Session session = null;
+                try {
+                    session = request.getSession(true);
+                } catch (LeaseException e) {
+                    throw new IOException(e);
+                }
+                session.put("current-user", user.getLogin());
+            }
+        }
+
+        response.setCode(Status.TEMPORARY_REDIRECT.getCode());
+        response.set("Location", "/");
+    }
+
 }
