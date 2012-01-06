e5b7e4b6bbdd75bd54cdcdc60df6307c3820c02e
François Sarradin <fsarradin@gmail.com>
2012-01-06 07:29:34 +0100

    modified page layout

--- src/main/java/net/kerflyn/broceliand/util/Templates.java
+++ src/main/java/net/kerflyn/broceliand/util/Templates.java
@@ -2,6 +2,8 @@ package net.kerflyn.broceliand.util;
 
 import com.google.common.io.Files;
 import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupDir;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,9 +12,12 @@ import static com.google.common.base.Charsets.UTF_8;
 
 public class Templates {
 
+    public static final STGroup TEMPLATE_GROUP = new STGroupDir("", "UTF-8", '$', '$');
+
     public static ST buildTemplate(String webpage) throws IOException {
         String raw = Files.toString(new File(webpage), UTF_8);
-        return new ST(raw, '$', '$');
+        ST template = new ST(raw, '$', '$');
+        return TEMPLATE_GROUP.createStringTemplateInternally(template);
     }
 
 }
