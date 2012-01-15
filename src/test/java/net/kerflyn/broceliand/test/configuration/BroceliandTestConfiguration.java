c5cd19dd5a8d0dd7a72a50ce0ebbae5bad02aac2
François Sarradin <fsarradin@gmail.com>
2012-01-15 19:49:20 +0100

    updated test configuration

--- src/test/java/net/kerflyn/broceliand/test/configuration/BroceliandTestConfiguration.java
+++ src/test/java/net/kerflyn/broceliand/test/configuration/BroceliandTestConfiguration.java
@@ -1,40 +1,31 @@
 package net.kerflyn.broceliand.test.configuration;
 
-import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
+import com.google.inject.Inject;
 import com.google.inject.Injector;
-import com.google.inject.Key;
+import com.google.inject.persist.PersistService;
+import com.google.inject.persist.jpa.JpaPersistModule;
 import net.kerflyn.broceliand.configuration.ControllerModule;
 import net.kerflyn.broceliand.configuration.RepositoryModule;
 import net.kerflyn.broceliand.configuration.ServiceModule;
-import net.kerflyn.broceliand.controller.BookController;
-import net.kerflyn.broceliand.controller.IndexController;
-import net.kerflyn.broceliand.controller.ResourceController;
-import net.kerflyn.broceliand.controller.UserController;
-import net.kerflyn.broceliand.repository.BasketElementRepository;
-import net.kerflyn.broceliand.repository.BookRepository;
-import net.kerflyn.broceliand.repository.UserRepository;
-import net.kerflyn.broceliand.repository.impl.BasketElementRepositoryImpl;
-import net.kerflyn.broceliand.repository.impl.BookRepositoryImpl;
-import net.kerflyn.broceliand.repository.impl.UserRepositoryImpl;
-import net.kerflyn.broceliand.service.BasketService;
-import net.kerflyn.broceliand.service.BookService;
-import net.kerflyn.broceliand.service.UserService;
-import net.kerflyn.broceliand.service.impl.BasketServiceImpl;
-import net.kerflyn.broceliand.service.impl.BookServiceImpl;
-import net.kerflyn.broceliand.service.impl.UserServiceImpl;
-import net.kerflyn.broceliand.util.persist.PersistModule;
-
-import static com.google.inject.name.Names.named;
 
 public class BroceliandTestConfiguration {
 
     public static Injector newGuiceInjector() {
-        Injector injector = Guice.createInjector(new PersistModule("test-manager-pu"),
+        Injector injector = Guice.createInjector(
+                new JpaPersistModule("test-manager-pu"),
                 new ControllerModule(),
                 new ServiceModule(),
                 new RepositoryModule());
+        injector.getInstance(PersistenceInitializer.class);
         return injector;
+    }
+
+    private static class PersistenceInitializer {
+        @Inject
+        public PersistenceInitializer(PersistService service) {
+            service.start();
+        }
     }
 
 }
