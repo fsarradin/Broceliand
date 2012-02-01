517746fe4f5c03c35ef828879d1e2cefdc0696c1
François Sarradin <fsarradin@gmail.com>
2012-02-01 01:13:09 +0100

    removed route binding with guice

--- src/main/java/net/kerflyn/broceliand/guiceroute/RouteExtensionVisitor.java
+++
@@ -1,9 +0,0 @@
-package net.kerflyn.broceliand.guiceroute;
-
-import com.google.inject.spi.BindingTargetVisitor;
-
-public interface RouteExtensionVisitor<T, V> extends BindingTargetVisitor<T, V> {
-
-    V visit(RouteBinding routeBinding);
-
-}
