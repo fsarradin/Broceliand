60dcb1d10c89d98eb5366e803c652e6d5a42a18c
François Sarradin <fsarradin@gmail.com>
2012-02-06 18:33:23 +0100

    added ugly code

--- src/main/java/net/kerflyn/broceliand/loader/BookLoader.java
+++ src/main/java/net/kerflyn/broceliand/loader/BookLoader.java
@@ -1,50 +1,66 @@
 package net.kerflyn.broceliand.loader;
 
 import com.google.common.base.Strings;
+import com.google.common.collect.Iterables;
+import net.kerflyn.broceliand.model.Book;
 
+import javax.xml.namespace.QName;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
+import java.io.Reader;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Lists.newLinkedList;
+import static com.google.common.collect.Maps.newHashMap;
+import static com.google.common.collect.Sets.newHashSet;
+import static javax.xml.stream.XMLStreamConstants.CHARACTERS;
 import static javax.xml.stream.XMLStreamConstants.END_ELEMENT;
 import static javax.xml.stream.XMLStreamConstants.START_ELEMENT;
 
 public class BookLoader {
 
-    public void parse(URL url) {
+    /**
+     * Get books by country, city, and seller.
+     *
+     * @param reader
+     * @return
+     */
+    public Map<String, Map<String, Map<String, Set<BookObject>>>> parse(Reader reader) {
+        Map<String, Map<String, Map<String, Set<BookObject>>>> result = newHashMap();
         XMLInputFactory factory = XMLInputFactory.newInstance();
-        InputStream inputStream = null;
-        try {
-            inputStream = url.openStream();
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
-        }
-
         XMLStreamReader streamReader = null;
+
         try {
-            streamReader = factory.createXMLStreamReader(inputStream);
-            int level = 0;
+            streamReader = factory.createXMLStreamReader(reader);
+            LinkedList<String> currentPath = newLinkedList();
 
             while (streamReader.hasNext()) {
                 int eventType = streamReader.next();
                 switch (eventType) {
                     case START_ELEMENT:
-                        System.out.println(Strings.repeat("  ", level)
-                                + streamReader.getName());
-                        level++;
+                        if ("books".equals(streamReader.getName().toString())) {
+                            Set<BookObject> books = readBooks(streamReader);
+                            addBooks(books, result, currentPath);
+                        }
+                        if (isInteresting(streamReader.getName())) {
+                            currentPath.push(streamReader.getAttributeValue("", "name"));
+                        }
                         break;
                     case END_ELEMENT:
-                        level--;
-                        break;
-                    case XMLStreamConstants.CHARACTERS:
-                        final String text = streamReader.getText().trim();
-                        if (!text.isEmpty()) {
-                            System.out.println(Strings.repeat(" ", level) + text);
+                        if (isInteresting(streamReader.getName())) {
+                            currentPath.pop();
                         }
+                        break;
                 }
             }
         } catch (XMLStreamException e) {
@@ -54,12 +70,73 @@ public class BookLoader {
                 if (streamReader != null) {
                     streamReader.close();
                 }
-                if (inputStream != null) {
-                    inputStream.close();
-                }
             } catch (Exception e) {
                 throw new IllegalStateException(e);
             }
         }
+        return result;
+    }
+
+    private void addBooks(Set<BookObject> books, Map<String, Map<String, Map<String, Set<BookObject>>>> storage, List<String> path) {
+        createPath(storage, path);
+        storage.get(path.get(2)).get(path.get(1)).get(path.get(0)).addAll(books);
+    }
+
+    private void createPath(Map<String, Map<String, Map<String, Set<BookObject>>>> storage, List<String> path) {
+        String country = path.get(2);
+        if (!storage.containsKey(country)) {
+            storage.put(country, new HashMap<String, Map<String, Set<BookObject>>>());
+        }
+
+        Map<String, Map<String, Set<BookObject>>> cities = storage.get(country);
+        String city = path.get(1);
+        if (!cities.containsKey(city)) {
+            cities.put(city, new HashMap<String, Set<BookObject>>());
+        }
+
+        Map<String, Set<BookObject>> sellers = cities.get(city);
+        String seller = path.get(0);
+        if (!sellers.containsKey(seller)) {
+            sellers.put(seller, new HashSet<BookObject>());
+        }
+    }
+
+    private Set<BookObject> readBooks(XMLStreamReader streamReader) throws XMLStreamException {
+        Set<BookObject> books = newHashSet();
+
+        int eventType = streamReader.next();
+        while (eventType != END_ELEMENT && !"books".equals(streamReader.getName().toString())) {
+            if (eventType == START_ELEMENT && "book".equals(streamReader.getName().toString())) {
+                BookObject book = readBook(streamReader);
+                books.add(book);
+            }
+            eventType = streamReader.next();
+        }
+
+        return books;
+    }
+
+    private BookObject readBook(XMLStreamReader streamReader) throws XMLStreamException {
+        BookObject book = new BookObject();
+
+        streamReader.next(); // START_ELEMENT: title
+        streamReader.next(); // CHARACTER: title
+        book.setTitle(streamReader.getText().trim());
+        streamReader.next(); // END_ELEMENT: title
+        streamReader.next(); // START_ELEMENT: author
+        streamReader.next(); // CHARACTERS: author
+        book.setAuthor(streamReader.getText().trim());
+        streamReader.next(); // END_ELEMENT: author
+        streamReader.next(); // START_ELEMENT: price
+        streamReader.next(); // CHARACTERS: price
+        book.setPrice(new BigDecimal(streamReader.getText().trim()));
+        streamReader.next(); // END_ELEMENT: price
+        streamReader.next(); // END_ELEMENT: book
+
+        return book;
+    }
+
+    private boolean isInteresting(QName name) {
+        return Iterables.contains(Arrays.asList("country", "city", "seller"), name.toString());
     }
 }
--- src/test/java/net/kerflyn/broceliand/loader/BookLoaderTest.java
+++ src/test/java/net/kerflyn/broceliand/loader/BookLoaderTest.java
@@ -1,18 +1,43 @@
 package net.kerflyn.broceliand.loader;
 
-import com.google.common.io.Resources;
+import net.kerflyn.broceliand.model.Book;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.net.URL;
+import java.io.StringReader;
+import java.util.Map;
+import java.util.Set;
+
+import static org.fest.assertions.Assertions.assertThat;
 
 public class BookLoaderTest {
 
+    private StringReader reader;
+
+    @Before
+    public void setUp() throws Exception {
+        String content = "<?xml version='1.0' encoding='UTF-8'?>" +
+                "<book-import-file>" +
+                "<country name='A Country'><city name='A City'><seller name='A Seller'><books><book>" +
+                "<title>A Title</title><author>An Author</author><price>1.00</price>" +
+                "</book></books></seller></city></country>" +
+                "</book-import-file>";
+        reader = new StringReader(content);
+    }
+
     @Test
-    public void should_load_books() {
+    public void should_load_books() throws Exception {
         BookLoader loader = new BookLoader();
 
-        URL fileUrl = Resources.getResource("loader/books.xml");
-        loader.parse(fileUrl);
+        Map<String, Map<String, Map<String, Set<BookObject>>>> countries = loader.parse(reader);
+        assertThat(countries.containsKey("A Country")).isTrue();
+        
+        Map<String, Map<String, Set<BookObject>>> cities = countries.get("A Country");
+        assertThat(cities.containsKey("A City")).isTrue();
+        
+        Map<String, Set<BookObject>> sellers = cities.get("A City");
+        assertThat(sellers.containsKey("A Seller")).isTrue();
+
     }
 
 }
