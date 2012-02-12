6fbec814bdf0e264ce76bbfb548a69103b992741
François Sarradin <fsarradin@gmail.com>
2012-02-12 04:45:44 +0100

    refactor SellerLoader

--- src/main/java/net/kerflyn/broceliand/loader/SellerLoader.java
+++ src/main/java/net/kerflyn/broceliand/loader/SellerLoader.java
@@ -2,13 +2,11 @@ package net.kerflyn.broceliand.loader;
 
 import com.google.common.collect.Iterables;
 
-import javax.xml.XMLConstants;
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import java.io.Reader;
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -24,6 +22,57 @@ import static javax.xml.stream.XMLStreamConstants.END_ELEMENT;
 import static javax.xml.stream.XMLStreamConstants.START_ELEMENT;
 
 public class SellerLoader {
+
+    private boolean isInteresting(QName name) {
+        return Iterables.contains(Arrays.asList("country", "city", "seller"), name.toString());
+    }
+
+    private String readBook(XMLStreamReader streamReader) throws XMLStreamException {
+        streamReader.next(); // CHARACTER: address
+        String address = streamReader.getText().trim();
+        streamReader.next(); // END_ELEMENT: address
+
+        return address;
+    }
+
+    private Set<String> readBooks(XMLStreamReader streamReader) throws XMLStreamException {
+        Set<String> addresses = newHashSet();
+
+        int eventType = streamReader.nextTag();
+        while (eventType != END_ELEMENT && !"addresses".equals(streamReader.getName().toString())) {
+            if (eventType == START_ELEMENT && "address".equals(streamReader.getName().toString())) {
+                String address = readBook(streamReader);
+                addresses.add(address);
+            }
+            eventType = streamReader.nextTag();
+        }
+
+        return addresses;
+    }
+
+    private void addBooks(Set<String> books, Map<String, Map<String, Map<String, Set<String>>>> storage, List<String> path) {
+        createPath(storage, path);
+        storage.get(path.get(2)).get(path.get(1)).get(path.get(0)).addAll(books);
+    }
+
+    private void createPath(Map<String, Map<String, Map<String, Set<String>>>> storage, List<String> path) {
+        String country = path.get(2);
+        if (!storage.containsKey(country)) {
+            storage.put(country, new HashMap<String, Map<String, Set<String>>>());
+        }
+
+        Map<String, Map<String, Set<String>>> cities = storage.get(country);
+        String city = path.get(1);
+        if (!cities.containsKey(city)) {
+            cities.put(city, new HashMap<String, Set<String>>());
+        }
+
+        Map<String, Set<String>> sellers = cities.get(city);
+        String seller = path.get(0);
+        if (!sellers.containsKey(seller)) {
+            sellers.put(seller, new HashSet<String>());
+        }
+    }
 
     /**
      * Get addresses by country, city, and seller.
@@ -71,56 +120,5 @@ public class SellerLoader {
             }
         }
         return result;
-    }
-
-    private void addBooks(Set<String> books, Map<String, Map<String, Map<String, Set<String>>>> storage, List<String> path) {
-        createPath(storage, path);
-        storage.get(path.get(2)).get(path.get(1)).get(path.get(0)).addAll(books);
-    }
-
-    private void createPath(Map<String, Map<String, Map<String, Set<String>>>> storage, List<String> path) {
-        String country = path.get(2);
-        if (!storage.containsKey(country)) {
-            storage.put(country, new HashMap<String, Map<String, Set<String>>>());
-        }
-
-        Map<String, Map<String, Set<String>>> cities = storage.get(country);
-        String city = path.get(1);
-        if (!cities.containsKey(city)) {
-            cities.put(city, new HashMap<String, Set<String>>());
-        }
-
-        Map<String, Set<String>> sellers = cities.get(city);
-        String seller = path.get(0);
-        if (!sellers.containsKey(seller)) {
-            sellers.put(seller, new HashSet<String>());
-        }
-    }
-
-    private Set<String> readBooks(XMLStreamReader streamReader) throws XMLStreamException {
-        Set<String> addresses = newHashSet();
-
-        int eventType = streamReader.nextTag();
-        while (eventType != END_ELEMENT && !"addresses".equals(streamReader.getName().toString())) {
-            if (eventType == START_ELEMENT && "address".equals(streamReader.getName().toString())) {
-                String address = readBook(streamReader);
-                addresses.add(address);
-            }
-            eventType = streamReader.nextTag();
-        }
-
-        return addresses;
-    }
-
-    private String readBook(XMLStreamReader streamReader) throws XMLStreamException {
-        streamReader.next(); // CHARACTER: address
-        String address = streamReader.getText().trim();
-        streamReader.next(); // END_ELEMENT: address
-
-        return address;
-    }
-
-    private boolean isInteresting(QName name) {
-        return Iterables.contains(Arrays.asList("country", "city", "seller"), name.toString());
     }
 }
