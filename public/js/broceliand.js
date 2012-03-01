066573d1a0691c74bea198b182a435f6f585c0a6
François Sarradin <fsarradin@gmail.com>
2012-03-02 00:38:37 +0100

    added possibility to modify the shipping charge list

--- public/js/broceliand.js
+++ public/js/broceliand.js
@@ -1,6 +1,15 @@
 
 jQuery.fn.exists = function() { return this.length > 0; }
 
+var IdGenerator = function() {
+    var id = 0;
+    this.newId = function() {
+        return id++;
+    };
+};
+
+var sessionIdGenerator = new IdGenerator();
+
 // add-modify-book.stg
 
 function priceModalDel(sellerId)  {
@@ -29,6 +38,8 @@ function priceModalSet(sellers) {
     }
 }
 
+// add-modify-seller.stg
+
 function shippingChargeModalSet(sellers) {
     var policy = $('#policy').val();
 //    var sellerId = $('#seller').val();
@@ -44,17 +55,31 @@ function match(value, obj) {
 
 function appendPolicy(policy) {
     match(policy, {
-        'None': function(policy) { return appendPolicyRow(policy, ""); },
-        'Fixed': function(policy) { return appendPolicyRow(policy, "<label>Price</label><input type='text'>"); },
-        'Proportional': function(policy) { return appendPolicyRow(policy, "<label>Rate</label><input type='text'>"); }
+        'None': function(policy) { return appendPolicyRow(policy, function(id) { return ""; }); },
+        'Fixed': function(policy) { return appendPolicyRow(policy, generatePriceInput); },
+        'Proportional': function(policy) { return appendPolicyRow(policy, generateRateInput); }
     });
 }
 
-function appendPolicyRow(policy, parameters) {
-    $("#policy-table").append("<tr>"
-        + "<td>" + policy + "</td>"
-        + "<td>" + parameters + "</td>"
-        + "<td><input type='text' class='span1'/></td>"
-        + "<td><a href='#' class='btn btn-danger'><i class='icon-trash icon-white'></i> Delete</a></td>"
+function generatePriceInput(id) {
+    return "<label>Price</label><input id='shipping-charge-price-" + id + "' type='text'>";
+}
+
+function generateRateInput(id) {
+    return "<label>Rate</label><input id='shipping-charge-rate-" + id + "' type='text'>";
+}
+
+function shippingChargeDel(id) {
+    $('#policy-table #' + id).remove();
+}
+
+function appendPolicyRow(policy, parameterGenerator) {
+    var id = sessionIdGenerator.newId();
+    var scId = "shipping-charge-" + id;
+    $("#policy-table").append("<tr id='" + scId + "'>"
+        + "<td><input id='shipping-charge-policy-" + id + "' type='hidden' value='" + policy + "'>" + policy + "</td>"
+        + "<td>" + parameterGenerator(id) + "</td>"
+        + "<td><input id='shipping-charge-quantity-" + id + "' type='text' class='span1'/></td>"
+        + "<td><a href='#' class='btn btn-danger' onclick=\"shippingChargeDel('" + scId + "')\"><i class='icon-trash icon-white'></i> Delete</a></td>"
         + "</tr>");
 }
