c98cfcafdaec10fb2572caadc7b58e641989dcfd
François Sarradin <fsarradin@gmail.com>
2012-03-02 07:18:46 +0100

    improved the resource controller

--- src/main/java/net/kerflyn/broceliand/controller/ResourceController.java
+++ src/main/java/net/kerflyn/broceliand/controller/ResourceController.java
@@ -9,22 +9,43 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.google.common.base.Joiner.on;
 
 public class ResourceController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ResourceController.class);
 
+    private static final Map<String, String> CONVERTER = new HashMap<String, String>() {{
+        put("css", "text/css");
+        put("js", "text/script");
+        put("jpg", "image/jpg");
+        put("png", "image/png");
+    }};
+
     @PathName(".*")
     public void index(Request request, Response response) throws IOException {
         String[] segments = request.getPath().getSegments();
 
-        String resourceType = segments[segments.length - 2];
-        String resourceName = segments[segments.length - 1];
+        String mimeType = getMimeType(segments[1]);
 
-        String resourcePath = "public/" + resourceType + "/" + resourceName;
+        String resourcePath = getResourcePath(segments);
         LOGGER.info("getting resource at \"{}\"", resourcePath);
 
+        response.set("Content-Type", mimeType);
         Files.copy(new File(resourcePath), response.getOutputStream());
+    }
+
+    private String getResourcePath(String[] segments) {
+        return "public/" + on("/").join(Arrays.copyOfRange(segments, 1, segments.length));
+    }
+
+    private String getMimeType(String segment) {
+        String type = segment.toLowerCase();
+        return CONVERTER.get(type);
     }
 
 }
